474ab99a8fdfc6e3df0757591b650664022c0fad
Micha-Btz <github@mdomann.tk>
2019-02-18 18:43:06 +0100

    update Android.mk

--- Android.mk
+++ Android.mk
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(zangyapro,$(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE), zanyapro)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
-
