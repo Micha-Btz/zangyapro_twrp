dbf9d8e43b5755e8955291f34e11e4c04540c92b
Micha-Btz <github@mdomann.tk>
2019-04-08 22:33:13 +0200

    update to pie source

--- recovery/gpt-utils/gpt-utils.cpp
+++ recovery/gpt-utils/gpt-utils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013,2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -51,7 +51,7 @@
 #include <vector>
 #include <string>
 #define LOG_TAG "gpt-utils"
-#include <cutils/log.h>
+#include <log/log.h>
 #include <cutils/properties.h>
 #include "gpt-utils.h"
 #include <endian.h>
@@ -193,11 +193,11 @@ static uint8_t *gpt_pentry_seek(const char *ptn_name,
 
     for (pentry_name = (char *) (pentries_start + PARTITION_NAME_OFFSET);
          pentry_name < (char *) pentries_end; pentry_name += pentry_size) {
-        char name8[MAX_GPT_NAME_SIZE / 2];
+        char name8[MAX_GPT_NAME_SIZE];
         unsigned i;
 
         /* Partition names in GPT are UTF-16 - ignoring UTF-16 2nd byte */
-        for (i = 0; i < sizeof(name8); i++)
+        for (i = 0; i < sizeof(name8) / 2; i++)
             name8[i] = pentry_name[i * 2];
         if (!strncmp(ptn_name, name8, len))
             if (name8[len] == 0 || !strcmp(&name8[len], BAK_PTN_NAME_EXT))
@@ -747,7 +747,6 @@ int prepare_partitions(enum boot_update_stage stage, const char *dev_path)
     enum gpt_state gpt_prim, gpt_second;
     enum boot_update_stage internal_stage;
     struct stat xbl_partition_stat;
-    struct stat ufs_dir_stat;
 
     if (!dev_path) {
         fprintf(stderr, "%s: Invalid dev_path\n",
@@ -970,7 +969,6 @@ int add_lun_to_update_list(char *lun_path, struct update_data *dat)
 
 int prepare_boot_update(enum boot_update_stage stage)
 {
-        int r, fd;
         int is_ufs = gpt_utils_is_ufs_device();
         struct stat ufs_dir_stat;
         struct update_data data;
@@ -1095,7 +1093,7 @@ int gpt_utils_get_partition_map(vector<string>& ptn_list,
         map<string, vector<string>>::iterator it;
         if (ptn_list.size() < 1) {
                 fprintf(stderr, "%s: Invalid ptn list\n", __func__);
-                return -1;
+                goto error;
         }
         //Go through the passed in list
         for (uint32_t i = 0; i < ptn_list.size(); i++)
@@ -1122,6 +1120,8 @@ int gpt_utils_get_partition_map(vector<string>& ptn_list,
                 memset(devpath, '\0', sizeof(devpath));
         }
         return 0;
+error:
+        return -1;
 }
 
 //Get the block size of the disk represented by decsriptor fd
@@ -1150,7 +1150,7 @@ static int gpt_set_header(uint8_t *gpt_header, int fd,
                 enum gpt_instance instance)
 {
         uint32_t block_size = 0;
-        off_t gpt_header_offset = 0;
+        off64_t gpt_header_offset = 0;
         if (!gpt_header || fd < 0) {
                 ALOGE("%s: Invalid arguments",
                                 __func__);
@@ -1170,7 +1170,7 @@ static int gpt_set_header(uint8_t *gpt_header, int fd,
                 ALOGE("%s: Failed to get gpt header offset",__func__);
                 goto error;
         }
-        ALOGI("%s: Writing back header to offset %ld", __func__,
+        ALOGI("%s: Writing back header to offset %" PRIi64, __func__,
                 gpt_header_offset);
         if (blk_rw(fd, 1, gpt_header_offset, gpt_header, block_size)) {
                 ALOGE("%s: Failed to write back GPT header", __func__);
